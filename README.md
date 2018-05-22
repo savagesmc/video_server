4d198a6735fd744c919776ed7c119cf544fe8964
Clark, Steve <steve@clarkhome.org>
2018-05-22 09:53:10 -0500

    Add options for setting bandwidth, and continous looping of the input file.

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ docker build -t <container_name> <this_folder>
 
 To use the container:
 ```bash
-docker run --rm -v $PWD:/myvideo.mp4:/tmp/myvideo.mp4 -p 8554:8554 -f /tmp/myvideo.mp4 -i rtsp://:8554/video
+docker run --rm -v $PDW:/myvideo.mp4:/tmp/myvideo.mp4 -p 8554:8554 <container_name> -f /tmp/myvideo.mp4 -i rtsp://:8554/video
 ```
--- simulate_video_stream.sh
+++ simulate_video_stream.sh
@@ -2,16 +2,20 @@
 # Two facultativ parameters : -f <nameOfTheVideoFile> and -i <URL_Output>
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P );
 sdp="rtsp://:8554/flux";
-while getopts "f:i:" OPTNAME
+loop=""
+bandwidth=0
+while getopts "f:i:b:l" OPTNAME
 do
    case $OPTNAME in
       f) name=$OPTARG;;
       i) sdp=$OPTARG;;
+      l) loop="--loop";;
+      b) bandwidth=$OPTARG;;
    esac
 done
 if [ -z $name ]; then
    echo "Counldn't open file - aborting."
    exit 255
 fi
-sout="#transcode{vb=0,scale=0,acodec=mpga,ab=128,channels=2,samplerate=44100}:rtp{sdp="$sdp"}";
-su vlcuser -c "vlc -vvv $name --sout '$sout'"
+sout="#transcode{vb=$bandwidth,scale=0,acodec=mpga,ab=128,channels=2,samplerate=44100}:rtp{sdp="$sdp"}";
+su vlcuser -c "cvlc -q -vvv $name $loop --sout '$sout'"
